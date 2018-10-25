7fd392ff1950416d03e62cf7d79dab3a9ec8f495
wts923 <wts923@tamu.edu>
2018-10-25 17:30:51 -0500

    works and has connect, disconnect messages

--- website/index.js
+++ website/index.js
@@ -8,8 +8,12 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+  io.emit('uinfo', {content:'user connected'});
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('disconnect', function(){
+    io.emit('uinfo', {content:'user disconnected'});
   });
 });
 
