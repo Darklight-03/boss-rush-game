e0209fcae53bc68262563277607729ad3557d512
Bo Corman <bdc3142@gmail.com>
2018-10-25 11:48:28 -0500

    basic movement

--- unity-project/Assets/Scripts/playerController.cs
+++ unity-project/Assets/Scripts/playerController.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
+  private Rigidbody2D rb;
 
 	// Use this for initialization
 	void Start () {
-		
+    rb = GetComponent<Rigidbody2D>();
 	}
+
+  // called in fixed interval
+  void FixedUpdate(){
+    // input x and y
+    float ix = Input.GetAxis("Horizontal");
+    float iy = Input.GetAxis("Vertical");
+    
+    // get velocity input
+    Vector2 inputvelocity = new Vector2(ix,iy);
+
+    // later can add velocity vectors together for knockback and stuff
+    rb.velocity = inputvelocity*0.5f;
+  }
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 }
