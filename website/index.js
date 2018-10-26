94615ce88eb481f5dbca924bf26f575f8fb67241
wts923 <wts923@tamu.edu>
2018-10-25 22:45:31 -0500

    changed file setup and used express.static to serve efficiently and safely

--- website/index.js
+++ website/index.js
@@ -1,23 +1,15 @@
-var app = require('express')();
+const express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index/index.html');
 });
 
-app.get('/index.css', function(req, res){
-  res.sendFile(__dirname + '/index.css');
-});
-
-app.get('/index-script.js', function(req, res){
-  res.sendFile(__dirname + '/index-script.js');
-});
-
-app.get('/assets/logo.png', function(req, res){
-  res.sendFile(__dirname + '/assets/logo.png');
-});
+app.use(express.static('index'));
+app.use(express.static('assets'));
 
 
 io.on('connection', function(socket){
