71e92a41787ad02cdab70cc72072b3ac64816de3
Jiahao Zhao <zhao626008414@tamu.edu>
2018-11-01 14:44:09 -0500

    boss updates

--- unity-project-bs/Assets/Scripts/GameManager.cs
+++ unity-project-bs/Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
   private Transform t;
+    public Transform canvas;
 
 	// Use this for initialization
 	void Start () {
@@ -12,8 +13,12 @@ public class GameManager : MonoBehaviour {
 	  GameObject obstacle1 = (GameObject)Instantiate(Resources.Load<GameObject>("rockspread"), t);
     //GameObject player = (GameObject)Instantiate(Resources.Load<GameObject>("Archer"),t);
     GameObject boss = (GameObject)Instantiate(Resources.Load<GameObject>("boss"),t);
-		
-	}
+        boss.transform.parent = canvas;
+        boss.GetComponent<RectTransform>().localScale = new Vector3(32.0f, 32.0f, 32.0f);
+        boss.GetComponent<RectTransform>().localPosition = Vector3.one;
+
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
