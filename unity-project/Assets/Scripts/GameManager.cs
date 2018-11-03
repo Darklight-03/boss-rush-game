fa94b7fdb59b4c41b3f3102d4eabe1b1b13bfbfb
wts923 <wts923@tamu.edu>
2018-11-02 23:11:46 -0500

    added snm to gamemanager and fixed null reference

--- unity-project/Assets/Scripts/GameManager.cs
+++ unity-project/Assets/Scripts/GameManager.cs
@@ -12,8 +12,8 @@ public class GameManager : MonoBehaviour {
     GameObject boss;
     GameObject obstacle1;
     private bool gameStarted = false;
-    private int[] waitPlayers;
-    private string[] waitPlayerIds;
+    private int[] waitPlayers = new int[0];
+    private string[] waitPlayerIds = new string[0];
 
     // Use this for initialization
     void Start () {
