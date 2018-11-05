3e7d9a155fb4da494ecdea0737691491bb9d4b87
w-kahlich <w_kahlich@tamu.edu>
2018-11-04 20:12:29 -0600

    Added start menu
    Now there is a Lobby Select screen and a Create Lobby screen

---
+++ unity-project/Assets/Menu_buttons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_buttons : MonoBehaviour { 
+    public GameObject MenuPanel;
+    public GameObject LobbySelectPanel;
+    public GameObject LobbyCreatePanel;
+
+    // Use this for initialization
+    void Start () 
+    {
+        MenuPanel.SetActive(true);
+        LobbySelectPanel.SetActive(false);
+        LobbyCreatePanel.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowLobbyPanel()
+    {
+        MenuPanel.SetActive(false);
+        LobbySelectPanel.SetActive(true);
+        LobbyCreatePanel.SetActive(false);
+    }
+
+    public void ShowMenuPanel()
+    {
+        MenuPanel.SetActive(true);
+        LobbySelectPanel.SetActive(false);
+        LobbyCreatePanel.SetActive(false);
+    }
+
+    public void ShowCreateLobbyPanel()
+    {
+        MenuPanel.SetActive(false);
+        LobbySelectPanel.SetActive(false);
+        LobbyCreatePanel.SetActive(true);
+    }
+
+
+
+}
