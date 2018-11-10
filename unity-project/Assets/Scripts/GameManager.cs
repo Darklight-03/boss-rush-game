8967d8328cd46dc705d0d90940fa61578f86d67b
wts923 <wts923@tamu.edu>
2018-11-10 02:10:17 -0600

    rearranged files a bit

--- unity-project/Assets/Scripts/GameManager.cs
+++ unity-project/Assets/Scripts/GameManager.cs
@@ -10,7 +10,7 @@ public class GameManager : MonoBehaviour {
     t = GetComponent<Transform>();
         Vector2 archerpos = new Vector2(2, -2);
         Vector2 bosspos = new Vector2(-2, 2);
-    GameObject player = (GameObject)Instantiate(Resources.Load<GameObject>("Archer"),archerpos,Quaternion.identity);
+    GameObject player = (GameObject)Instantiate(Resources.Load<GameObject>("Knight"),archerpos,Quaternion.identity);
 	 // GameObject obstacle1 = (GameObject)Instantiate(Resources.Load<GameObject>("rockspread"), t);
     //GameObject player = (GameObject)Instantiate(Resources.Load<GameObject>("Archer"),t);
     //GameObject boss = (GameObject)Instantiate(Resources.Load<GameObject>("boss"),bosspos,Quaternion.identity,t);
