91a4a43bdf8292e67425048ff89bc1fce8b1073e
wts923 <wts923@tamu.edu>
2018-11-11 16:07:35 -0600

    temporarily made all players spawn as knights

--- unity-project/Assets/Scripts/GameManager.cs
+++ unity-project/Assets/Scripts/GameManager.cs
@@ -69,7 +69,7 @@ public class GameManager : MonoBehaviour {
         // argument will specify class later
         if (player2 == null)
         {
-            player2 = Instantiate(Resources.Load<GameObject>(playerClasses[cl]), playerInitPos[num], Quaternion.identity);
+            player2 = Instantiate(Resources.Load<GameObject>(playerClasses[1]), playerInitPos[num], Quaternion.identity);
             Debug.Log("instantiate " + id + " at " + playerInitPos[num].ToString());
             player2.GetComponent<archerControllerOP>().playernum = num;
             player2.GetComponent<archerControllerOP>().id = id;
@@ -77,7 +77,7 @@ public class GameManager : MonoBehaviour {
         }
         else if (player3 == null)
         {
-            player3 = Instantiate(Resources.Load<GameObject>(playerClasses[cl]), playerInitPos[num], Quaternion.identity);
+            player3 = Instantiate(Resources.Load<GameObject>(playerClasses[1]), playerInitPos[num], Quaternion.identity);
             Debug.Log("instantiate " + id + " at " + playerInitPos[num].ToString());
             player3.GetComponent<archerControllerOP>().playernum = num;
             player3.GetComponent<archerControllerOP>().id = id;
