f694cb8c7cc83a3dbdf79b611f3de9ac3943faa2
wts923 <wts923@tamu.edu>
2018-11-15 16:39:08 -0600

    server changes, networking fixed but arrows are very broken rather than fixed

--- website/app.js
+++ website/app.js
@@ -1,15 +1,22 @@
-const express = require('express')
-const app = express()
-var path = require('path')
-var bodyParser = require('body-parser')
-var compression = require('compression')
+const fs = require('fs');
+const util = require('util');
+const express = require('express');
+const app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var compression = require('compression');
 var http = require('http').Server(app);
 const port = 3000
 const unityFolder = path.join(__dirname, 'unity-build');
 var WebSocket = require('ws');
 var io = new WebSocket.Server( { server: http } );
 
- 
+process.env.TZ = 'America/Chicago';
+var log_file = fs.createWriteStream(__dirname + '/messages.log', {flags: 'w'});
+function log_msg(sock, d) {
+  log_file.write((new Date()) + " - " + sock.id + ": " + util.format(d) + '\n');
+}
+
 app.use(bodyParser.json());
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
@@ -21,91 +28,111 @@ app.use(express.static('assets'));
 
 http.listen(port);
 
-
 function defaultRoute(req, response) {
-	    response.sendfile(unityFolder + '/index.html');
+  response.sendfile(unityFolder + '/index.html');
 }
 
 function extraStringify(inobj) {
   return "\"" + JSON.stringify(inobj) + "\""; 
 }
 
-//console.log(io);
-
 var lobbies = [];
 
 io.on('connection', function(socket){
-  //console.log(socket);
   socket.id = getUniqueID();
-  console.log('user connected');
+  socket.inlobby = false;
   socket.send(JSON.stringify({ msgtype: 'new connection', content: JSON.stringify({ yourid: socket['id'] }) }));
 
+	console.log((new Date()) + ' user ' + socket.id + ' connected');
+	log_msg(socket, "connected");
+
   socket.on('message', function (msgs) {
+
     console.log(msgs.toString('utf8'));
-    msg = JSON.parse(msgs.toString('utf8'));
+    log_msg(socket, msgs.toString('utf8'));
+		msg = JSON.parse(msgs.toString('utf8'));
+		
     switch (msg['msgtype']) {
+
       case 'create lobby':
-        // msg { }
-        var lobby = [ { sock: socket } ];
-        var thislobbyid = lobbies.length;
-        lobbies[thislobbyid] = lobby;
-		    console.log(0 + " " + socket['id']);
-        socket.send(JSON.stringify({ msgtype: 'create lobby', content: JSON.stringify({ playernum: 0, lobbyid: thislobbyid }) }));
+        if (socket.inlobby) {
+          // fail
+          socket.send(JSON.stringify({ msgtype: 'create lobby', content: JSON.stringify({ playernum: -1, lobbyid: -1 }) }));
+  			}
+        else {
+					// success
+					socket.inlobby = true;
+					var lobby = { name: msg['name'], members: [ socket ] };
+					var thislobbyid = lobbies.length;
+					lobbies[thislobbyid] = lobby;
+					socket.send(JSON.stringify({ msgtype: 'create lobby', content: JSON.stringify({ playernum: 0, lobbyid: thislobbyid }) }));
+        }
       break;
 
       case 'join lobby':
-        // msg { lobbyid:14 }
         var lobby = lobbies[msg['lobbyid']];
-        if (lobby == undefined) {
-          socket.send(JSON.stringify({ msgtype: 'join lobby', content: JSON.stringify({ playernum: -1, ret:'fail' }) }));
+        if (lobby == undefined || lobby.members.length >= 3 || socket.inlobby) {
+          socket.send(JSON.stringify({ msgtype: 'join lobby', content: JSON.stringify({ lobbyid: -1, playernum: -1, ret:'fail' }) }));
         }
         else {
-	  for (i = 0; i < lobby.length; i++) {
-		  console.log(lobby.length + " " + socket['id']);
-            lobby[i]['sock'].send(JSON.stringify({ msgtype: 'new player', content: JSON.stringify({ theirnum: lobby.length, theirid: socket['id'], cl: 0 }) }));
-		  console.log(i + " " + lobby[i]['sock']['id']);
-	    socket.send(JSON.stringify({ msgtype: 'new player', content: JSON.stringify({ theirnum: i, theirid: lobby[i]['sock']['id'], cl: 0 }) }));
-	  }
-          lobby[lobby.length] = { sock: socket };
+			// send messages to each lobby member about the new member
+			// send messages to the new member about each lobby member
+			for (i = 0; i < lobby.members.length; i++) {
+				console.log("sent to " + lobby.members[i]['id']);
+				lobby.members[i].send(JSON.stringify({ msgtype: 'new player', content: JSON.stringify({ theirnum: lobby.members.length, theirid: socket['id'], cl: 0 }) }));
+				console.log("sent to " + socket.id + "-");
+				socket.send(JSON.stringify({ msgtype: 'new player', content: JSON.stringify({ theirnum: i, theirid: lobby.members[i]['id'], cl: 0 }) }));
+			}
+			socket.inlobby = true;
+          lobby.members[lobby.members.length] = socket;
           lobbies[msg['lobbyid']] = lobby;
-          socket.send(JSON.stringify({ msgtype: 'join lobby', content: JSON.stringify({ playernum: lobby.length - 1, ret: 'success' }) }));
+          socket.send(JSON.stringify({ msgtype: 'join lobby', content: JSON.stringify({ lobbyid: msg['lobbyid'], playernum: lobby.members.length - 1, ret: 'success' }) }));
         }
       break;
 
       case 'disband lobby':
-        // msg { lobbyid:14 }
         lobbies.splice(msg['lobbyid'], 1);
         socket.send(JSON.stringify({ msgtype: 'disband lobby', content: JSON.stringify({ ret: 'success' }) }));
       break;
 
       case 'general message':
-        // msg { lobbyid:14, content:"hello there" }
         var lobby = lobbies[msg['lobbyid']];
         if (lobby != null) {
-          for (i = 0; i < lobby.length; i++) {
-            if (lobby[i]['sock']['id'] != socket['id']) {
-              lobby[i]['sock'].send(JSON.stringify({ msgtype: 'general message', content: JSON.stringify({ sender: socket['id'], ct: msg['ct'], content: JSON.stringify(msg['content']) }) }));
-	    }
+          for (i = 0; i < lobby.members.length; i++) {
+            if (lobby.members[i]['id'] != socket['id']) {
+              lobby.members[i].send(JSON.stringify({ msgtype: 'general message', content: JSON.stringify({ sender: socket['id'], ct: msg['ct'], content: JSON.stringify(msg['content']) }) }));
+      }
           }
         }
       break;
 
       case 'get lobbies':
-        socket.send(JSON.stringify({ msgtype: 'get lobbies', content: JSON.stringify({ lob: lobbies }) }));
+        var lobbiesInfo = [];
+        for (i = 0; i < lobbies.length; i++) {
+          var lobbyInfo = {};
+          var lobby = lobbies[i];
+          for (j = 0; j < lobby.members.length; j++) {
+           
+    			}
+					lobbyInfo['players'] = lobby.members.length;
+					lobbyInfo['name'] = lobby.name;
+          lobbiesInfo[lobbiesInfo.length] = lobbyInfo;
+        }
+        socket.send(JSON.stringify({ msgtype: 'get lobbies', content: JSON.stringify({ lobbiesInfo: lobbiesInfo }) }));
       break;
 
     }
-    console.log(lobbies);
   });
 
   socket.on('close', function(reasonCode, description) {
-    console.log((new Date()) + ' Peer ' + socket.remoteAddress + ' disconnected.');
+    console.log((new Date()) + ' user ' + socket.id + ' disconnected');
+    log_msg(socket, "disconnected");
     var lobsl = lobbies.length;
     for (i = 0; i < lobsl; i++) {
-      var lobl = lobbies[i].length;
+      var lobl = lobbies[i].members.length;
       for (j = 0; j < lobl; j++) {
-        if (lobbies[i][j]['sock']['id'] == socket['id']) {
-          lobbies[i].splice(j, 1);
+        if (lobbies[i].members[j]['id'] == socket['id']) {
+          lobbies[i].members.splice(j, 1);
           lobl--;
           break;
         }
@@ -118,7 +145,6 @@ io.on('connection', function(socket){
     }
     console.log(lobbies);
   });
-
 });
 
 
